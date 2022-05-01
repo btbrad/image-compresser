08678245bbf32b377c3da5757b7b981cf1a503bc
btbrad <biantaobrad@gmail.com>
2022-05-01 23:39:40 +0800

    校验文件类型

--- compress.js
+++ compress.js
@@ -1,9 +1,16 @@
+const ACCEPT = ['image/jpg', 'image/jpeg', 'image/png']
+
 const fileInput = document.querySelector('#file')
 
 fileInput.addEventListener('change', function(e) {
-  console.log(e)
-})
-
-fileInput.addEventListener('input', function(e) {
-  console.log(e)
+  const [file] = e.target.files
+  console.log(file)
+  if (!file) {
+    return
+  }
+  const { type: fileType, size: fileSize } = file
+  if (!ACCEPT.includes(fileType)) {
+    alert(`不支持[${fileType}]文件类型！`)
+    return
+  }
 })
