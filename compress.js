59ab4a460210cab02b82b6160e383e3ffafc0d6b
btbrad <biantaobrad@gmail.com>
2022-05-01 23:02:33 +0800

    initial commit

---
+++ compress.js
@@ -0,0 +1,9 @@
+const fileInput = document.querySelector('#file')
+
+fileInput.addEventListener('change', function(e) {
+  console.log(e)
+})
+
+fileInput.addEventListener('input', function(e) {
+  console.log(e)
+})
